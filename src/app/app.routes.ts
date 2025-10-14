90c0b46d7f30caf67ec6fd5c2792efd8b65db3e8
Tarun Garg <tarungrg0001@gmail.com>
2025-10-14 15:39:01 +0530

    SMS-24 #time 3h #comment assignment header add #done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Assignments } from './components/assignments/assignments';
 
-export const routes: Routes = [{ path: 'dashboard', component: Dashboard, pathMatch: 'full' }];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'assignments', component: Assignments },
+];
