5eff78f59b0a4aafe663bf7e414abcb227239698
Tarun Garg <tarungrg0001@gmail.com>
2025-11-03 09:48:40 +0530

    SMS-33 #t 12h user form complete

--- src/app/components/crm-users/crm-user/crm-user.ts
+++ src/app/components/crm-users/crm-user/crm-user.ts
@@ -2,15 +2,26 @@ import { Component, effect } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 import { Resource } from '../../../services/resource';
+import { CommonModule } from '@angular/common';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'crm-crm-user',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, FontAwesomeModule, RouterLink],
   templateUrl: './crm-user.html',
   styleUrl: './crm-user.scss',
 })
 export class CrmUser {
   public userContent: any;
+  public readonly errorIcon = faExclamationCircle;
+
+  public user: { name: string | null; email: string | null; mobile: number | null } = {
+    name: null,
+    email: null,
+    mobile: null,
+  };
 
   constructor(private _resource: Resource) {
     effect(() => {
@@ -18,5 +29,7 @@ export class CrmUser {
     });
   }
 
-  public onSubmit(form: NgForm) {}
+  public onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
